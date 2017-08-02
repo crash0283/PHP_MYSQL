0004fa7d6d7ecfc263c8e8813259b64668e492d2
rasch <rasch83@gmail.com>
2017-08-02 11:12:20 -0500

    starting part 2 of CMS;  added logo, new index.php, public header and footer

---
+++ globe_bank/private/shared/public_footer.php
@@ -0,0 +1,10 @@
+  <footer>
+    <p>Copyright <?php echo date('Y'); ?>, Globe Bank International </p>
+  </footer>
+
+  </body>
+</html>
+
+<?php
+  db_disconnect($db);
+?>
--- globe_bank/public/index.php
+++ globe_bank/public/index.php
@@ -1,18 +1,13 @@
-<?php
-    //Bring in all the php pages we need though the init file
-    require_once('../private/init.php');
-?>
+<?php require_once('../private/init.php'); ?>
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Globe Bank</title>
-</head>
-<body>
-    <h1>Globe Bank: Coming Soon!</h1>
-</body>
-</html>
+<?php include(SHARED_PATH . '/public_header.php'); ?>
+
+<div id="main">
+
+  <div id="page">
+
+  </div>
+
+</div>
+
+<?php include(SHARED_PATH . '/public_footer.php'); ?>
